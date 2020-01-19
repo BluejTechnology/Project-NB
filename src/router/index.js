15b4cba651789f18c8ebba2da379da2c022bec35
sallypanda <920756154@qq.com>
2020-01-19 19:48:21 +0800

    add: 解决冲突

--- src/router/index.js
+++ src/router/index.js
@@ -5,37 +5,26 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-<<<<<<< HEAD
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/testApi',
-    name: 'testApi',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/testApi.vue')
-=======
-    path: '/download',
-    name: 'download',
-    component: () => import('../views/DownloadPage.vue')
->>>>>>> 08731adb89c261b19bc3d7386c4e3f94ec7984f5
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/testApi',
+        name: 'testApi',
+        component: () => import(/* webpackChunkName: "about" */ '../views/testApi.vue')
+    },
+
+    {
+        path: '/download',
+        name: 'download',
+        component: () => import('../views/DownloadPage.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,23 @@
-
-const path = require('path')
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@src': path.resolve(__dirname, 'src'),
-                '@static': path.resolve(__dirname, 'static')
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+    devServer: {
+        // host:'qzi.html5.qq.com',
+        proxy: {
+            '/':{
+                target:'https://qzi.html5.qq.com',
+                ws: true,
+                changeOrigin: true,
             }
-        },
-        devServer: {
-            // 设置代理，方便本地开发的时候，规避跨域问题，5001是server端那边的运行接口
-            proxy: {
-                '/fcgi': {
-                    target: 'https://qzi.html5.qq.com',
-                    changeOrigin: true
-                },
-
-            },
-            https: true,
-            port: 443,
-            disableHostCheck: true // 解决 Invalid Host header
-        },
-        css: {
-            loaderOptions: {
-                // 但是在配置 `data` 选项的时候
-                // `scss` 语法会要求语句结尾必须有分号，`sass` 则要求必须没有分号
-                // 在这种情况下，我们可以使用 `scss` 选项，对 `scss` 语法进行单独配置
-                scss: {
-                    prependData: `@import "~@/assets/scss/util.scss";`
-                }
+        }
+    },
+    css:{
+        loaderOptions:{
+            // 但是在配置 `data` 选项的时候
+            // `scss` 语法会要求语句结尾必须有分号，`sass` 则要求必须没有分号
+            // 在这种情况下，我们可以使用 `scss` 选项，对 `scss` 语法进行单独配置
+            scss:{
+            prependData: `@import "~@/assets/scss/util.scss";`
             }
         }
     }
 }
-
