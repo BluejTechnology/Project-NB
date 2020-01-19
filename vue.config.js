ebbfb44a625c52ea398c88f57dc34bc62d4c546f
zongqiu <1026439166@qq.com>
2020-01-19 19:45:16 +0800

    add: 上传测试头像

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  devServer: {
-	// host:'qzi.html5.qq.com',
-    proxy: {
-		'/':{
-			target:'https://qzi.html5.qq.com',
-			ws: true,
-			changeOrigin: true,
+	publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+	devServer: {
+		// host:'qzi.html5.qq.com',
+		proxy: {
+			'/':{
+				target:'https://qzi.html5.qq.com',
+				ws: true,
+				changeOrigin: true,
+			}
+		}
+	},
+	css:{
+		loaderOptions:{
+			// 但是在配置 `data` 选项的时候
+			// `scss` 语法会要求语句结尾必须有分号，`sass` 则要求必须没有分号
+			// 在这种情况下，我们可以使用 `scss` 选项，对 `scss` 语法进行单独配置
+			scss:{
+			prependData: `@import "~@/assets/scss/util.scss";`
+			}
 		}
 	}
-  },
-  css:{
-	  loaderOptions:{
-		// 但是在配置 `data` 选项的时候
-		// `scss` 语法会要求语句结尾必须有分号，`sass` 则要求必须没有分号
-		// 在这种情况下，我们可以使用 `scss` 选项，对 `scss` 语法进行单独配置
-		scss:{
-		  prependData: `@import "~@/assets/scss/util.scss";`
-		}
-	  }
-  }
 }
