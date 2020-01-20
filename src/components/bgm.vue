d82bf1004521d695af6aad82fe7088c811ba620b
cool <randyho@foxmail.com>
2020-01-21 05:01:10 +0800

    fix_audio

--- src/components/bgm.vue
+++ src/components/bgm.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bgm_wrapper" @click="switchPlay">
 		<i class="btn_music" :class="{'sound-on':isPlay,'sound-off':!isPlay,'active':isPlay}"></i>
-		<video src="../assets/test_bgm.mp3" preload autoplay loop ref="sound0"></video>
+		<video src="../assets/test_bgm.mp3" preload autoplay loop ref="sound0" playsinline="" webkit-playsinline="" x5-playsinline=""  x-webkit-airplay="allow"></video>
 	</div>
 </template>
 
