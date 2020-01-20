2435a17f18e22fb3aea2c853bf25b09373e3eb42
区家宝 <oujiabao@bggou-mb3.tencent.com>
2020-01-21 05:01:43 +0800

    增加sentry

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+// 前端sentry监控
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+process.env.isBuild && Sentry.init({
+    dsn: 'https://266aeebd126448cfb530211d02ff9825@report.url.cn/sentry/2163',
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+    logErrors: true,
+    release: `qzi`
+})
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
